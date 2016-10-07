8e9308c21d34d2d8c2b315c7d93f036b824706eb
Scott Jackson <daneren2005@gmail.com>
2016-10-06 17:42:35 -0700

    Convert away from apache.http and over to HttpUrlConnection

--- src/github/daneren2005/serverproxy/ServerProxy.java
+++ src/github/daneren2005/serverproxy/ServerProxy.java
@@ -21,19 +21,19 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
 import java.math.BigInteger;
+import java.net.HttpURLConnection;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.SocketTimeoutException;
+import java.net.URL;
 import java.net.URLDecoder;
 import java.net.URLEncoder;
 import java.net.UnknownHostException;
 import java.nio.ByteOrder;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.StringTokenizer;
-
-import org.apache.http.Header;
-import org.apache.http.HttpRequest;
-import org.apache.http.message.BasicHttpRequest;
 
 import android.content.Context;
 import android.net.wifi.WifiManager;
@@ -137,43 +137,49 @@ public abstract class ServerProxy implements Runnable {
 		protected Socket client;
 		protected String path;
 		protected int cbSkip = 0;
-		protected HttpRequest request;
+		protected Map<String, String> requestHeaders = new HashMap<>();
 
 		public ProxyTask(Socket client) {
 			this.client = client;
 		}
 
-		protected HttpRequest readRequest() {
-			HttpRequest request = null;
+		protected boolean readRequest() {
 			InputStream is;
 			String firstLine;
-			BufferedReader reader = null;
+			BufferedReader reader;
 			try {
 				is = client.getInputStream();
 				reader = new BufferedReader(new InputStreamReader(is), 8192);
 				firstLine = reader.readLine();
 			} catch (IOException e) {
 				Log.e(TAG, "Error parsing request", e);
-				return request;
+				return false;
 			}
 
 			if (firstLine == null) {
 				Log.i(TAG, "Proxy client closed connection without a request.");
-				return request;
+				return false;
 			}
 
 			StringTokenizer st = new StringTokenizer(firstLine);
 			if(!st.hasMoreTokens()) {
 				Log.w(TAG, "Unknown request with no tokens");
-				return request;
+				return false;
 			} else if(st.countTokens() < 2) {
 				Log.w(TAG, "Unknown request with no uri: \"" + firstLine + '"');
-				return request;
+				return false;
 			}
 			String method = st.nextToken();
 			String uri = st.nextToken();
 			String realUri = uri.substring(1);
-			request = new BasicHttpRequest(method, realUri);
+
+			// Process path
+			try {
+				path = URLDecoder.decode(realUri, "UTF-8");
+			} catch (UnsupportedEncodingException e) {
+				Log.e(TAG, "Unsupported encoding", e);
+				return false;
+			}
 
 			// Get all of the headers
 			try {
@@ -184,7 +190,8 @@ public abstract class ServerProxy implements Runnable {
 					if(index != -1 && (index + 2) < line.length()) {
 						String headerName = line.substring(0, index);
 						String headerValue = line.substring(index + 2);
-						request.addHeader(headerName, headerValue);
+
+						requestHeaders.put(headerName, headerValue);
 					}
 				}
 			} catch(IOException e) {
@@ -193,30 +200,18 @@ public abstract class ServerProxy implements Runnable {
 				Log.w(TAG, "Exception reading request", e);
 			}
 
-			return request;
+			return true;
 		}
 
 		public boolean processRequest() {
-			request = readRequest();
-			if (request == null) {
+			if (!readRequest()) {
 				return false;
 			}
-
-			// Read HTTP headers
-			try {
-				path = URLDecoder.decode(request.getRequestLine().getUri(), "UTF-8");
-			} catch (UnsupportedEncodingException e) {
-				Log.e(TAG, "Unsupported encoding", e);
-				return false;
-			}
-
 			Log.i(TAG, "Processing request for " + path);
 
 			// Try to get range requested
-			Header rangeHeader = request.getFirstHeader("Range");
-
-			if(rangeHeader != null) {
-				String range = rangeHeader.getValue();
+			String range = requestHeaders.get("Range");
+			if(range != null) {
 				int index = range.indexOf("=");
 				if(index >= 0) {
 					range = range.substring(index + 1);
--- src/github/daneren2005/serverproxy/WebProxy.java
+++ src/github/daneren2005/serverproxy/WebProxy.java
@@ -18,38 +18,32 @@ package github.daneren2005.serverproxy;
 import android.content.Context;
 import android.util.Log;
 
-import org.apache.http.Header;
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.HttpStatus;
-import org.apache.http.StatusLine;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.DefaultHttpClient;
-
 import java.io.BufferedOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.net.HttpURLConnection;
 import java.net.Socket;
-import java.util.ArrayList;
+import java.net.URL;
 import java.util.Arrays;
-import java.util.Iterator;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import javax.net.ssl.HostnameVerifier;
 
 public class WebProxy extends ServerProxy {
 	private static String TAG = WebProxy.class.getSimpleName();
 	private static List REMOVE_REQUEST_HEADERS = Arrays.asList("Host", "Accept-Encoding", "Referer");
 	private static List REMOVE_RESPONSE_HEADERS = Arrays.asList("Transfer-Encoding");
-	private HttpClient httpClient;
+	private HostnameVerifier hostnameVerifier;
 
 	public WebProxy(Context context) {
 		super(context);
 	}
-	public WebProxy(Context context, HttpClient httpClient) {
+	public WebProxy(Context context, HostnameVerifier hostnameVerifier) {
 		super(context);
-
-		this.httpClient = httpClient;
+		this.hostnameVerifier = hostnameVerifier;
 	}
 
 	@Override
@@ -58,25 +52,32 @@ public class WebProxy extends ServerProxy {
 	}
 
 	protected class StreamSiteTask extends ProxyTask {
-		private List<Header> headers;
+		private Map<String, String> responseHeaders;
 
 		public StreamSiteTask(Socket client) {
 			super(client);
 		}
 
-		public List<Header> getHeaders(List<Header> headers) {
-			// Strip common problems
-			Iterator<Header> it = headers.iterator();
-			while(it.hasNext()) {
-				Header header = it.next();
-				if("Server".equals(header.getName())) {
-					it.remove();
+		public Map<String, String> getHeaders(Map<String, List<String>> rawHeaders) {
+			Map<String, String> headers = new HashMap<>();
+			for(Map.Entry<String, List<String>> entry: rawHeaders.entrySet()) {
+				String name = entry.getKey();
+				List<String> values = entry.getValue();
+				String value;
+				if(values.isEmpty()) {
+					value = "";
+				} else {
+					value = values.get(0);
+				}
+
+				if(!"Server".equals(name)) {
+					headers.put(name, value);
 				}
 			}
 
 			return headers;
 		}
-		private String getHeaderString(int response, List<Header> headers) {
+		private String getHeaderString(int response, Map<String, String> headers) {
 			StringBuilder sb = new StringBuilder();
 
 			sb.append("HTTP/1.0 ");
@@ -84,22 +85,22 @@ public class WebProxy extends ServerProxy {
 			sb.append(" OK\r\n");
 
 			boolean addContentType = true;
-			for(Header header: headers) {
-				if(REMOVE_RESPONSE_HEADERS.contains(header.getName())) {
+			for(Map.Entry<String, String> header: headers.entrySet()) {
+				if(REMOVE_RESPONSE_HEADERS.contains(header.getKey())) {
 					continue;
 				}
 
-				sb.append(header.getName());
+				sb.append(header.getKey());
 				sb.append(": ");
 
 				// Make sure that connection is close, not keep-alive
-				if("Connection".equals(header.getName())) {
+				if("Connection".equals(header.getKey())) {
 					sb.append("close");
 				} else {
 					sb.append(header.getValue());
 				}
 
-				if("Content-Type".equals(header.getName())) {
+				if("Content-Type".equals(header.getKey())) {
 					addContentType = false;
 				}
 
@@ -115,41 +116,27 @@ public class WebProxy extends ServerProxy {
 
 		@Override
 		public void run() {
-			HttpClient httpClient;
-			if(WebProxy.this.httpClient != null) {
-				httpClient = WebProxy.this.httpClient;
-			} else {
-				httpClient = new DefaultHttpClient();
-			}
-			HttpResponse response;
 			try {
-                HttpPost newRequest = new HttpPost(path);
-				for(Header header: request.getAllHeaders()) {
-					if(!REMOVE_REQUEST_HEADERS.contains(header.getName()) && !(header.getName().equals("Content-Length") && header.getValue().equals("0"))  ) {
-						newRequest.addHeader(header);
+				// Open new connection to destination and add existing headers
+				URL url = new URL(path);
+				HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+				for(Map.Entry<String, String> header: requestHeaders.entrySet()) {
+					if(!REMOVE_REQUEST_HEADERS.contains(header.getKey()) && !("Content-Length".equals(header.getKey()) && "0".equals(header.getValue()))  ) {
+						connection.setRequestProperty(header.getKey(), header.getValue());
 					}
 				}
 
-				response = httpClient.execute(newRequest);
-				StatusLine status = response.getStatusLine();
-				if(status.getStatusCode() == HttpStatus.SC_OK || status.getStatusCode() == HttpStatus.SC_PARTIAL_CONTENT) {
-					headers = new ArrayList<Header>();
-					headers.addAll(Arrays.asList(response.getAllHeaders()));
-					headers = getHeaders(headers);
+				if(connection.getResponseCode() == HttpURLConnection.HTTP_OK || connection.getResponseCode() == HttpURLConnection.HTTP_PARTIAL) {
+					responseHeaders = getHeaders(connection.getHeaderFields());
 
 					OutputStream output = null;
 					InputStream input = null;
 					try {
 						output = new BufferedOutputStream(client.getOutputStream(), 64*1024);
-						output.write(getHeaderString(response.getStatusLine().getStatusCode(), headers).getBytes());
+						output.write(getHeaderString(connection.getResponseCode(), responseHeaders).getBytes());
 
-						HttpEntity entity = response.getEntity();
-						if(entity == null) {
-							Log.w(TAG, "Failed to get entity for request: " + path);
-							return;
-						}
-						input = entity.getContent();
 
+						input = connection.getInputStream();
 						byte[] buffer = new byte[1024 * 32];
 						int count = 0, n= 0;
 						while (-1 != (n = input.read(buffer))) {
@@ -175,8 +162,8 @@ public class WebProxy extends ServerProxy {
 						}
 					}
 				} else {
-					response.getEntity().getContent().close();
-					throw new IOException(status.getReasonPhrase());
+					connection.disconnect();
+					throw new IOException(connection.getResponseMessage());
 				}
 			} catch (IOException e) {
 				Log.e(TAG, "Failed to get data from url: " + path, e);
